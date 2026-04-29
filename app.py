69f3ce24d1a21f126043dd027a049a27e691e787
Anisabet Ginting <gintinganisabet333@gmail.com>
2026-04-29 22:18:54 +0700

    Update app.py
    HItung luas lingkaran

--- app.py
+++ app.py
@@ -6,3 +6,11 @@ def luas_segitiga():
     print("Luas segitiga adalah: ", luas)
 
 luas_segitiga()
+# Hitung Luas Persegi Panjang
+def luas_persegi_panjang():
+    p = int(input("Masukkan panjang persegi: "))
+    l = int(input("Masukkan lebar persegi: "))
+    luas = p * l
+    print("Luas persegi adalah: ", luas)
+
+luas_persegi_panjang()
